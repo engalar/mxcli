9137444eccece84ed59fa23865ad712e5f26f3be
eg <liuwengao@foxmail.com>
2026-07-24 10:35:00 +0800

    feat: add mxcli fix grid-sources command
    Converts Forms to CustomWidgets
    on DataGrid2 widgets using BSON-to-JSON roundtrip. Fixes CE1571 errors
    caused by stale microflow-based datasources that should be XPath/database-from.

--- cmd/mxcli/main.go
+++ cmd/mxcli/main.go
@@ -438,6 +438,7 @@ func init() {
 	rootCmd.AddCommand(exprCmd)
 	rootCmd.AddCommand(buildCmd())
 	rootCmd.AddCommand(runCmd())
+	rootCmd.AddCommand(fixCmd)
 	rootCmd.AddCommand(reloadCmd())
 	rootCmd.AddCommand(mockCmd())
 	rootCmd.AddCommand(syntaxCmd)
